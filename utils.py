15719356e744a843f29a4d9878babbffbe060b9e
axellee1994 <axellee1994@gmail.com>
2026-04-22 13:28:29 +0800

    Retrieved back utils.py

---
+++ utils.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+
+def is_path_dir(dir_path: str) -> None:
+    """
+    Check is path a valid directory
+    """
+    base_path = Path(dir_path)
+
+    if not base_path.exists():
+        raise FileNotFoundError(f"Path does not exist: {dir_path}")
+
+    if not base_path.is_dir():
+        raise NotADirectoryError(f"Path is not a directory: {dir_path}")
+
+
+def is_image_file(filepath: Path) -> None:
+    """
+    Check filepath is an image file (.jpg, .jpeg)
+    """
+    if not filepath.is_file():
+        raise FileNotFoundError(f"file not found: {filepath}")
+
+    if filepath.suffix.lower() not in (".jpg", ".jpeg"):
+        raise ValueError("file must be a .jpg or .jpeg image")
